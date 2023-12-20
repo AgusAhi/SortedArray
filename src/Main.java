f07d4493a408f6bcfcaad970a0e9843f8a41aeb1
aad <aguahidol@alu.edu.gva.es>
2023-12-20 16:19:23 +0100

    up

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        SortedArray sortedArray = new SortedArray(10);
+        SortedArray sortedArray = new SortedArray(5);
         sortedArray.put(2);
         sortedArray.put(8);
         sortedArray.put(4);
         sortedArray.put(12);
         sortedArray.put(5);
+        System.out.println("---SortedArray---");
         System.out.println(sortedArray);
-        sortedArray.put(3);
+
+        System.out.println("---getNumElements---");
+        System.out.println(sortedArray.getNumElements());
+
+        System.out.println("---getElementsAt---");
         System.out.println(sortedArray);
+
+        System.out.println("I want position 2:");
+        System.out.println(sortedArray.getElementAt(2));
+
+        System.out.println("---removeElementsAt---");
+        sortedArray.removeElemtent(2);
+        System.out.println(sortedArray);
+
+        System.out.println("---isEmpty---");
+        System.out.println(sortedArray);
+        System.out.println(sortedArray.isEmpty());
+
+        System.out.println("---isFull---");
+        System.out.println(sortedArray);
+        System.out.println(sortedArray.isFull()); // false because we removed with the removeElement
+
+        System.out.println("---existElement---");
+        System.out.println(sortedArray.existsElement(2));
+
+        System.out.println("---existElementBinarySearch---");
+        System.out.println(sortedArray.existsElementBinarySearch(2));
+
+
 
     }
 }
--- src/SortedArray.java
+++ src/SortedArray.java
@@ -1,8 +1,8 @@
 public class SortedArray {
     private int[] array;
-    private int numElements;
+    public int numElements;
 
-    public SortedArray(int size) {
+    public SortedArray(int size) { // Initialize the array
         array = new int[size];
 
         for (int i = 0; i < size; i++) {
@@ -11,7 +11,7 @@ public class SortedArray {
         numElements = 0;
     }
 
-    public void put(int value) {
+    public void put(int value) { // Put the elements on the Array
         int i = numElements - 1;
         while (i >= 0 && array[i] > value) {
             if (i + 1 < array.length) {
@@ -25,14 +25,19 @@ public class SortedArray {
         }
     }
 
-@Override
-    public String toString() {
+    @Override
+    public String toString() { // Print the array
         String result = "[";
-        for (int i = 0; i < numElements - 1; i++) {
-            result += array[i] + ", ";
+        boolean first = true;
+        for (int i = 0; i < numElements; i++) {
+            if (first) {
+                first = false;
+            } else {
+                result += ", ";
+            }
+            result += array[i];
         }
-        result += array[numElements - 1] + "]";
-        return result;
+        return result += "]";
     }
 
     public int getNumElements() {
@@ -43,17 +48,69 @@ public class SortedArray {
         return array.length;
     }
 
-    public void getElementAt(int index) {
+    public int getElementAt(int index) {
         if (index < 0 || index >= numElements) {
             throw new IndexOutOfBoundsException(
-                "SortedArray.getElementAt: index out of bounds: " +
-                        index + " out of bounds. Max index: " + (numElements - 1)
+                    "SortedArray.getElementAt: index out of bounds: " +
+                            index + " out of bounds. Max index: " + (numElements - 1));
+        }
+        return array[index];
+    }
+
+    public void removeElemtent(int index) {
+        if (index < 0 || index >= numElements) {
+            throw new IndexOutOfBoundsException(
+                    "SortedArray.getElementAt: index out of bounds: " +
+                            index + " out of bounds. Max index: " + (numElements - 1)
             );
         }
+        int value = array[index];
         for (int i = index; i < numElements - 1; i++) {
             array[i] = array[i + 1];
         }
         numElements--;
- 
     }
+
+    public boolean isEmpty() {
+        return numElements == 0;
+    }
+
+    public boolean isFull() {
+        return numElements == array.length;
+    }
+
+    public boolean existsElement(int number) {
+        for (int i = 0; i < numElements; i++) {
+            if (array[i] == number) {
+                return true;
+            } else {
+                if (array[i] > number) {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Binary Search ut8.5 pag 18
+    public boolean existsElementBinarySearch(int number) {
+        return binarySearch(number, 0, numElements - 1);
+    }
+
+    public boolean binarySearch(int number, int minorIndex, int upperIndex) {
+        if (minorIndex > upperIndex) {
+            return false;
+        }
+        int middle = (minorIndex + upperIndex) / 2;
+        if (array[middle] == number) {
+            return true;
+        }
+        if (array[middle] < number) {
+            return binarySearch(number, middle + 1, upperIndex);
+        } else {
+            return binarySearch(number, minorIndex, middle - 1);
+        }
+
+    }
+
 }

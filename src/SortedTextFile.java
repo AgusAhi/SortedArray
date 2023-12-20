ede0cbc068e00043e1d3fb29327ddec7dd94e32b
aad <aguahidol@alu.edu.gva.es>
2023-12-20 17:11:36 +0100

    upe

--- src/SortedTextFile.java
+++ src/SortedTextFile.java
@@ -1,2 +1,31 @@
+
+import java.io.*;
+
 public class SortedTextFile {
+    private String fileName;
+
+
+    public SortedTextFile(String text) throws IOException {
+        this.fileName = fileName;
+    }
+
+    public String getName() { // return the name
+        return fileName;
+    }
+
+    public void put() throws IOException { // that will store the string into the file but in such a way that the file is sorted in ascending order.
+        File file = new File(fileName);
+        PrintWriter out = new PrintWriter(new FileWriter(file));
+
+        try {
+            String line;
+
+
+        }
+
+    }
+
+
+
+
 }
